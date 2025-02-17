2bb02ad3930dae34ab4938ba605afd99c4565b34
olisimmonds <102732150+olisimmonds@users.noreply.github.com>
2025-02-17 18:23:24 +0000

    ADD: model struct

--- src/models/DeepAR/model_train_deepar.py
+++ src/models/DeepAR/model_train_deepar.py
@@ -1,31 +1,36 @@
-from __future__ import print_function
-
-%matplotlib inline
-
-import sys
-import zipfile
-from dateutil.parser import parse
-import json
-from random import shuffle
-import random
-import datetime
-import os
-
-import boto3
-import s3fs
-import sagemaker
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import timedelta
+import json
+from gluonts.mx import SimpleFeedForwardEstimator, Trainer
 
-# from ipywidgets import interact, interactive, fixed, interact_manual
-# import ipywidgets as widgets
-# from ipywidgets import IntSlider, FloatSlider, Checkbox
+hyperparameters = {
+    # we use 5 min frequency for the time series
+    "time_freq": "5m",
+    # we predict for 3o mins
+    "prediction_length": 30/5,
+    # we use a context of 3 hours
+    "context_length": 180/5,
+    "epochs": "400",
+    "early_stopping_patience": "40",
+    "mini_batch_size": "64",
+    "learning_rate": "5E-4",
+    "num_hidden_dimensions": [10]
+}
 
-# we use 5 min frequency for the time series
-freq = "5m"
-# we predict for 3o mins
-prediction_length = 30/5
-# we use a context of 3 hours
-context_length = 180/5
+
+estimator = SimpleFeedForwardEstimator(
+    num_hidden_dimensions=hyperparameters["num_hidden_dimensions"],
+    prediction_length=hyperparameters["prediction_length"],
+    context_length=hyperparameters["context_length"],
+    trainer=Trainer(ctx="cpu", epochs=hyperparameters["epochs"], learning_rate=hyperparameters["learning_rate"], num_batches_per_epoch=hyperparameters["mini_batch_size"]),
+)
+
+# Path to file in the parent directory
+file_path = '../deepar_train_data.jsonl'
+
+# Open and read the JSONL file
+with open(file_path, "r") as file:
+    training_data = [json.loads(line) for line in file]
+
+predictor = estimator.train(training_data)
