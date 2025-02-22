4eb6245a5a063f000cc2e25c51a5f6a417e57a2c
olisimmonds <102732150+olisimmonds@users.noreply.github.com>
2025-02-22 10:28:01 +0000

    Improved DeepAR model

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Algorithmic Trading Bot
-My attempt at using AI to make short-term predictions on the stock market. Early stages as of now, just using a LSTM model to predict the financial time series but I intend to introduce strategies to the model shortly.
+My attempt at using AI to make short-term predictions on the stock market. This currently has two models, a simple LSTM model and a more complex DeepAR model. DeepAR can use multiple time series as input and produces a probabilistic forecast making it ideal for predicting stocks. I am looking to implement strategies and improve the performance of the DeepAR model.
--- src/models/DeepAR/model_train_deepar.py
+++ src/models/DeepAR/model_train_deepar.py
@@ -1,55 +1,87 @@
-import numpy as np
+import json
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-import json
-import torch
-# from gluonts.torch.model.deepar import DeepAREstimator
-# from gluonts.torch. import Trainer
-# from gluonts.torch.model.simple_feedforward import SimpleFeedForwardEstimator
+
+import numpy as np
+np.bool = np.bool_
+import mxnet as mx
+
 from gluonts.torch import DeepAREstimator
-# estimator = SimpleFeedForwardEstimator(prediction_length=6)
+from gluonts.dataset.common import ListDataset
+from gluonts.mx import Trainer
+from gluonts.evaluation import make_evaluation_predictions
+import random
 
-# # Open and read the JSONL file
-# with open("deepar_train_data.jsonl", "r") as file:
-#     training_data = [json.loads(line) for line in file]
+# Load JSONL file
+jsonl_file = "deepar_train_data.jsonl"
 
-# predictor = estimator.train(training_data[0])
+data = []
+with open(jsonl_file, "r") as f:
+    for line in f:
+        data.append(json.loads(line))
 
-# hyperparameters = {
-#     # we use 5 min frequency for the time series
-#     "time_freq": "5m",
-#     # we predict for 30 mins
-#     "prediction_length": 30 // 5,
-#     # we use a context of 3 hours
-#     "context_length": 180 // 5,
-#     "epochs": 400,
-#     "early_stopping_patience": 40,
-#     "mini_batch_size": 64,
-#     "learning_rate": 5E-4,
-#     "num_hidden_dimensions": [10]
-# }
+random.shuffle(data)
 
-# estimator = SimpleFeedForwardEstimator(prediction_length=6)
-    # freq=hyperparameters["time_freq"],
-    # num_hidden_dimensions=hyperparameters["num_hidden_dimensions"],
-     #hyperparameters["prediction_length"],
-    # context_length=hyperparameters["context_length"]
-    # trainer=Trainer(
-    #     ctx=torch.device("cpu"),
-    #     epochs=hyperparameters["epochs"],
-    #     learning_rate=hyperparameters["learning_rate"],
-    #     batch_size=hyperparameters["mini_batch_size"],
-    #     early_stopping_patience=hyperparameters["early_stopping_patience"],
-    # ),
-# )
+split_idx = int(0.8 * len(data))
 
-# Path to file in the parent directory
-# file_path = os.path.join("..", "..", "..", "deepar_train_data.jsonl")
-# file_path = '../deepar_train_data.jsonl'
+# Train and test sets
+train_data = data[:split_idx]
+test_data = data[split_idx:]
 
-# # Open and read the JSONL file
-# with open("deepar_train_data.jsonl", "r") as file:
-#     training_data = [json.loads(line) for line in file]
-# # print(training_data[0])
-# predictor = estimator.train(training_data[0])
+def normalize_relative(series):
+    """
+    Normalize a time series to start at 1 and represent changes as multiples of the initial value.
+    
+    Args:
+        series: The original time series.
+    
+    Returns:
+        Normalized series starting at 1.
+    """
+
+    series = np.array(series)
+    if type(series[0]) == "numpy.ndarray":
+        return [series[0] / series[0][0]]
+    else:
+        prod = series / series[0]
+        return prod
+
+# Convert to ListDataset format
+train_dataset = ListDataset(
+    [{
+        "target": normalize_relative(entry["target"]), 
+        "start": pd.Timestamp(entry["start"]), 
+        "dynamic_feat": normalize_relative(entry["dynamic_feat"])
+        } for entry in train_data],
+    freq="5min"
+)
+
+# Convert to ListDataset format
+test_dataset = ListDataset(
+    [{
+        "target": normalize_relative(entry["target"]), 
+        "start": pd.Timestamp(entry["start"]), 
+        "dynamic_feat": normalize_relative(entry["dynamic_feat"])
+        } for entry in test_data],
+    freq="5min"
+)
+
+# Train the model and make predictions
+model = DeepAREstimator(
+    prediction_length=6, freq="5min", num_batches_per_epoch = 40, trainer_kwargs={"max_epochs": 100}
+).train(train_dataset)
+
+forecast_it, ts_it = make_evaluation_predictions(
+    dataset=train_dataset,  # test dataset
+    predictor=model,  # predictor
+    num_samples=100,  # number of sample paths we want for evaluation
+)
+forecasts = list(forecast_it)
+tss = list(ts_it)
+
+for i in range(10):
+    plt.figure(figsize=(8, 5)) 
+    plt.plot(tss[i].to_timestamp())
+    forecast_entry = forecasts[i]
+    forecast_entry.plot(show_label=True)
+    plt.legend()
